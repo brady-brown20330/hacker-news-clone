8e05b99f6e4096d79025caf753a6f0f0a9c1dd90
Brady Brown <brownb202@gmail.com>
2021-04-20 12:17:27 -0600

    app.jsx is now a functional component, and user can now switch between top and new posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,72 @@ import { getPosts } from '../utils/api';
 import axios from 'axios';
 import { Feed } from './components/Feed';
 
-// const App = () => {
-//   const [posts, setPosts] = useState(null)
+const App = () => {
+  const [posts, setPosts] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-//   useEffect(() => {
-//     getPosts('top')
-//     .then(data => setPosts(data))
-//   }, [])
-  
-//   return (
-//     <div>
-//       <button onClick={() => {console.log('posts in state: ', posts)}}>Click Me</button>
-//       <Feed posts={posts}/>
-//     </div>
-//   )
-// }
-
-class App extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      posts: null,
-      loading: true
-    }
-  }
-
-  render() {
-    return (
-     <div className='app'>
-        <Feed posts={this.state.posts} loading={this.state.loading}/>
-     </div>
-    )
-  }
-
-
-  componentDidMount() {
+  useEffect(() => {
     getPosts('top')
-    .then(data => {
-      this.setState({
-        posts: data, 
-        loading: false
-      })
-    }
-    )
-  }
+    .then(data => setPosts(data))
+    .then(setLoading(false))
+  }, [])
+
+const fetchPosts = (type) => {
+  setLoading(true)
+  getPosts(type)
+  .then(data => {
+    setPosts(data)
+  })
+  .then(setLoading(false))
 }
+  
+  return (
+    <div>
+      <button onClick={() => {fetchPosts('top')}}>Top</button>
+      <button onClick={() => {fetchPosts('new')}}>New</button>
+      <Feed posts={posts} loading={loading}/>
+    </div>
+  )
+}
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+//       posts: null,
+//       loading: true
+//     }
+//     const fetchPosts = (type) => {
+//       getPosts(type)
+//       .then(data => {
+//         this.setState({
+//           posts: data
+//         })
+//       })
+//     }
+
+//   }
+
+//   render() {
+//     return (
+//      <div className='app'>
+//        <button onClick={() => {this.fetchPosts('new').bind(this)}}>New</button>
+//         <Feed posts={this.state.posts} loading={this.state.loading}/>
+//      </div>
+//     )
+//   }
+
+
+//   componentDidMount() {
+//     getPosts('top')
+//     .then(data => {
+//       this.setState({
+//         posts: data, 
+//         loading: false
+//       })
+//     })
+//   }
+// }
 
 export default App;
